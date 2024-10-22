6737d9b46e771aaaedc2f9344d5eba2ff93b9bee
yash7xm <yash.7xm@gmail.com>
2024-10-22 18:56:45 +0530

    tcp transport listen and accept added

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
@@ -17,4 +18,32 @@ func NewTCPTransport(listenAddr string) *TCPTransport {
 	return &TCPTransport{
 		listenAddress: listenAddr,
 	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error: %s\n", err)
+		}
+
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("New incoming connection %+v\n", conn)
 }
--- p2p/transport.go
+++ p2p/transport.go
@@ -6,4 +6,6 @@ type Peer interface{}
 // Transport is anything that handles the communtication
 // between the nodes in the network. This can be of the
 // form (TCP, UDP, websockets, ...)
-type Transport interface{}
+type Transport interface {
+	ListenAndAccept() error
+}
