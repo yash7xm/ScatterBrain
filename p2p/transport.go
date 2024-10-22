86f122ddd53f8677d49f2864a518d786432b5f40
yash7xm <yash.7xm@gmail.com>
2024-10-22 18:32:02 +0530

    transport interface added

---
+++ p2p/transport.go
@@ -0,0 +1,9 @@
+package p2p
+
+// Peer is an interface that represents the remote node
+type Peer interface{}
+
+// Transport is anything that handles the communtication
+// between the nodes in the network. This can be of the
+// form (TCP, UDP, websockets, ...)
+type Transport interface{}
